b9b66393a474ea7cc11893a59fbf8698dfd35077
David Luhr <davidc.luhr@gmail.com>
2017-04-28 16:28:57 -0600

    Add hammerjs setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {RouterModule, Routes} from '@angular/router'
-import {MaterialModule} from '@angular/material'
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router'
+import { MaterialModule } from '@angular/material'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
-import {myFirebaseConfig, myFirebaseAuthConfig } from './firebase.config';
+import { myFirebaseConfig, myFirebaseAuthConfig } from './firebase.config';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarComponent } from './car/car.component';
-import {AppRoutes} from './routes'
+import { AppRoutes } from './routes'
 import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { HeaderComponent } from './header/header.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-
-
 
 @NgModule({
   declarations: [
