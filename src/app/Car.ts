7ab08427b053e2a5154dce2e3a0b19b595ce4221
troygerber <troy.gerber@gmail.com>
2017-05-02 20:41:15 -0600

    Working with firebase data.

--- src/app/Car.ts
+++ src/app/Car.ts
@@ -1,3 +1,4 @@
 export class Car {
     name: string;
+    owner: string;
 }
--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFire, FirebaseListObservable} from 'angularfire2'
-import {Observable} from 'rxjs'
+import { Component, OnInit, Inject } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseRef} from 'angularfire2';
+import {database} from 'firebase';
+import {Observable} from 'rxjs';
 import "rxjs/add/operator/filter";
-import 'rxjs/add/operator/map'
-import { Car } from '../Car'
+import 'rxjs/add/operator/map';
+import { Car } from '../Car';
 
 @Component({
   selector: 'pd-car-list',
@@ -13,21 +14,36 @@ import { Car } from '../Car'
 export class CarListComponent implements OnInit {
 
   // items: Observable<Car>;
-  items: FirebaseListObservable<any>
+  items: FirebaseListObservable<any>;
+  uid: string;
+  carRef;
 
-  constructor(af: AngularFire){
+  constructor(af: AngularFire, @Inject(FirebaseRef) fb){
+    this.carRef = fb.database().ref("/cars");
 
     af.auth.subscribe(authData => {
     console.log(authData);
-    let uid = authData.uid;
+    this.uid = authData.uid;
     this.items = af.database.list('cars', {
         query: {
             orderByChild: 'owner',
-            equalTo: uid // currentUser.uid
+            equalTo: this.uid // currentUser.uid
         }
     });
 });//this.items = af.database.list('cars');
   }
+  add(){
+    
+    //let ref = this.fb.database().ref("/cars");
+  //   ref.push(car)
+  let car = new Car();
+  car.name = "Troy2";
+  car.owner = this.uid;
+  //var cars = this.items.push(car);
+   //ref.push(car);
+   this.carRef.push(car);
+
+}
   // constructor(af: AngularFire) {
   //   this.items = af.database.list('cars').map( items =>{
   //     return items.filter(item => (<Car>item).name ==='Test car 2');
